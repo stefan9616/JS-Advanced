b04d73a2e5784c2b04a47a565a926f8773e65127
stefan9616 <143064670+stefan9616@users.noreply.github.com>
2023-08-25 00:38:30 +0300

    Add files via upload

---
+++ JS-Apps-ExamPrepare/src/services/authService.js
@@ -0,0 +1,51 @@
+import * as request from './requester.js';
+
+const baseUrl = 'http://localhost:3030/users';
+
+const saveUser = (user) => {
+
+    if (user.accessToken) {
+        localStorage.setItem('user', JSON.stringify(user))
+    }
+}
+const deleteUser = () => {
+    localStorage.removeItem('user');
+}
+
+export const getUser = () => {
+    let serializedUser = localStorage.getItem('user');
+
+    if (serializedUser) {
+        let user = JSON.parse(serializedUser);
+        return user;
+    }
+}
+const getToken = () => getUser()?.accessToken;
+
+
+
+
+export const register = (email, password) =>
+    request.post(`${baseUrl}/register`, { email, password })
+        .then(user => {
+            saveUser(user);
+            return user;
+        });
+
+export const login = (email, password) =>
+    request.post(`${baseUrl}/login`, { email, password })
+        .then(user => {
+            saveUser(user);
+            return user;
+        });
+
+
+export const logout = () =>
+    fetch(`${baseUrl}/logout`, { headers: { 'X-Authorization': getToken()} })
+        .then(() => {
+            deleteUser();
+        });
+
+
+
+
